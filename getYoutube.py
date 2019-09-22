b4a252c08499ef5ea48115703fd4d979a2b831e2
Ben Lerchin and Erika Pacheco <pair+ben+erika@ministryofvelocity.com>
2019-09-21 17:28:26 -0700

    use playlistitems endpoint instead of search

--- getYoutube.py
+++ getYoutube.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 from dotenv import load_dotenv
-from json import dumps
+from json import dumps 
 from requests import get
-from time import strptime
+from time import strptime 
 from os.path import isfile, join, dirname
 from os import environ
 
@@ -11,19 +11,19 @@ if isfile(dotenv_path):
     load_dotenv(dotenv_path)
 
 API_KEY = environ.get('API_KEY');
-CHANNEL_ID = environ.get('CHANNEL_ID');
+PLAYLIST_ID = environ.get('PLAYLIST_ID');
 
 FILTER_BEFORE = datetime(2019, 1, 1)
 DATA_PATH = 'data/videos.json'
 
-def get_request_url(api_key, channel_id, page_token=None):
-    url = 'https://www.googleapis.com/youtube/v3/search?key=%s&channelId=%s&part=snippet,id&maxResults=50&order=date' % (api_key, channel_id)
+def get_request_url(api_key, playlist_id, page_token=None):
+    url = 'https://www.googleapis.com/youtube/v3/playlistItems?key=%s&playlistId=%s&part=snippet,id&order=date&maxResults=50' % (api_key, playlist_id)
     if page_token:
         url += '&pageToken=%s' % page_token
     return url
 
 def get_page(page_token=None):
-    r = get(get_request_url(API_KEY, CHANNEL_ID, page_token))
+    r = get(get_request_url(API_KEY, PLAYLIST_ID, page_token))
     if r.status_code == 200:
         return r.json()
     else:
@@ -36,7 +36,7 @@ def get_videos():
     while True:
         page = get_page(next_token)
         videos += page['items']
-        if 'nextPageToken' in page:
+        if len(page['items']) > 0 and 'nextPageToken' in page:
             next_token = page['nextPageToken']
         else:
             break;
