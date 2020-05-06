8bbf4556f3b454ccf163cc8b8e6b43d9a88a793e
Ben Lerchin <ben@lerchin.com>
2020-05-06 15:29:33 -0700

    allow timestamp with or without ms

--- getYoutube.py
+++ getYoutube.py
@@ -43,7 +43,10 @@ def get_videos():
     return videos
 
 def parse_timestamp(timestamp):
-    return datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%SZ')
+        try:
+            return datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S.%fZ')
+        except ValueError:
+            return datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%SZ')
 
 def filter_videos(videos):
     return [v for v in videos if parse_timestamp(v['snippet']['publishedAt']) > FILTER_BEFORE]
